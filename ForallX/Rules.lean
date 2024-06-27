9f61bfd53af9208a2d930a30c82a71ee1a9854bf
Francisco J. Piedrahita-Velez <fpiedrah@brown.edu>
2024-06-27 11:13:00 -0400

    Add rules from Chapter 19 and complete section exercises.

--- ForallX/Rules.lean
+++ ForallX/Rules.lean
@@ -1,3 +1,10 @@
+theorem disjunctive_syllogism {P Q: Prop} : ((P ∨ Q) ∧ ¬P) → Q := by
+  intro h₁
+  obtain ⟨s₁, s₂⟩ := h₁
+  cases s₁ with
+  | inr h₂ => exact h₂
+  | inl h₂ => contradiction
+
 theorem modus_ponens {P Q: Prop} : ((P → Q) ∧ P) → Q := by
   intro h₁
   obtain ⟨s₁, s₂⟩ := h₁
@@ -12,9 +19,73 @@ theorem modus_tollens {P Q: Prop} : ((P → Q) ∧ ¬Q) → ¬P := by
   apply s₂
   exact s₃
 
+theorem double_negation {P: Prop} : ¬¬P → P := by
+  intro h₁
+  by_cases h₂: P
+  . exact h₂
+  . contradiction
+
+--  ???
 theorem excluded_middle {P: Prop} : P ∨ ¬P := by
   by_cases h₁: P
   . left
     exact h₁
   . right
     exact h₁
+
+theorem de_morgans_and {P Q: Prop} :  ¬(P ∧ Q) ↔ ¬P ∨ ¬Q := by
+  constructor
+  . intro h₁
+    by_cases h₂: P
+    . right
+      intro h₃
+      apply h₁
+      constructor
+      . exact h₂
+      . exact h₃
+    . left
+      exact h₂
+  . intro h₁
+    intro h₂
+    obtain ⟨s₁, s₂⟩ := h₂
+    cases h₁ with
+    | inr h₃ => contradiction
+    | inl h₃ => contradiction
+
+theorem de_morgans_or {P Q: Prop} :  ¬(P ∨ Q) ↔ ¬P ∧ ¬Q := by
+  constructor
+  . intro h₁
+    constructor
+    . intro h₂
+      apply h₁
+      left
+      exact h₂
+    . intro h₂
+      apply h₁
+      right
+      exact h₂
+  . intro h₁
+    intro h₂
+    obtain ⟨s₁, s₂⟩ := h₁
+    cases h₂ with
+    | inr h₃ => contradiction
+    | inl h₃ => contradiction
+
+theorem or_commutative {P Q: Prop} : (P ∨ Q) ↔ (Q ∨ P) := by
+  constructor
+  . intro h₁
+    cases h₁ with
+    | inr h₂ =>
+      . left
+        exact h₂
+    | inl h₂ =>
+      . right
+        exact h₂
+  . intro h₁
+    cases h₁ with
+    | inr h₂ =>
+      . left
+        exact h₂
+    | inl h₂ =>
+      . right
+        exact h₂
