9389d3ef45b757488f0fa7c35e3f1b38653bf6b0
Francisco J. Piedrahita-Velez <fpiedrah@brown.edu>
2024-06-27 13:19:55 -0400

    Add rules from Chapter 19 and complete section exercises.

---
+++ ForallX/Chapter19.lean
@@ -0,0 +1,44 @@
+import Mathlib.Init.Classical
+import ForallX.Rules
+
+
+example {E F G: Prop} (p₁: E ∨ F) (p₂: F ∨ G) (p₃: ¬F) : E ∧ G := by
+  rw [or_commutative] at p₁
+  have s₁: E := disjunctive_syllogism ⟨p₁, p₃⟩
+  have s₂: G := disjunctive_syllogism ⟨p₂, p₃⟩
+  constructor
+  . exact s₁
+  . exact s₂
+
+example {M N: Prop} (p₁: M ∨ (N → M)) : ¬M → ¬N := by
+  intro h₁
+  cases p₁ with
+  | inr h₂ =>
+    . have s₁: ¬N := modus_tollens ⟨h₂, h₁⟩
+      exact s₁
+  | inl h₂ => contradiction
+
+example {M N O P: Prop}
+    (p₁: (M ∨ N) ∧ (O ∨ P))
+    (p₂: N → P)
+    (p₃: ¬P) :
+    M ∧ O := by
+  obtain ⟨s₁, s₂⟩ := p₁
+  rw [or_commutative] at s₁
+  rw [or_commutative] at s₂
+  have s₃: ¬N := modus_tollens ⟨p₂, p₃⟩
+  have s₄: M := disjunctive_syllogism ⟨s₁, s₃⟩
+  have s₅: O := disjunctive_syllogism ⟨s₂, p₃⟩
+  constructor
+  . exact s₄
+  . exact s₅
+
+example {X Y Z D: Prop}
+    (p₁: (X ∨ Y) ∨ (X ∨ Z))
+    (p₂: ¬(X ∨ D))
+    (p₃: D ∨ M) :
+    M := by
+  rw [de_morgans_or] at p₂
+  obtain ⟨_, s₁⟩ := p₂
+  have s₃: M := disjunctive_syllogism ⟨p₃, s₁⟩
+  exact s₃
